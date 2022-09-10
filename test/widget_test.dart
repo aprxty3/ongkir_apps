3c8b3c99fa3c81e887759c0e6470cb0872ba8bb8
aprxty3 <ajiprxty3@gmail.com>
2022-09-10 13:46:32 +0700

    mapping data

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,8 +1,12 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 
 void main() async {
   Uri url = Uri.parse('https://reqres.in/api/users/2');
   final res = await http.get(url);
 
-  print(res);
+  final data = (jsonDecode(res.body) as Map<String, dynamic>)["data"];
+
+  print(data["first_name"] + " " + data["last_name"]);
 }
