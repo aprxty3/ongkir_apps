b6bfe57e937fbe386c3e2a9310e296cab071174e
aprxty3 <ajiprxty3@gmail.com>
2022-09-10 23:16:55 +0700

    test api

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,11 +4,9 @@ import 'package:http/http.dart' as http;
 import 'package:ongkir_apps/app/data/models/user_models.dart';
 
 void main() async {
-  Uri url = Uri.parse('https://reqres.in/api/users');
-  final res = await http.post(url, body: {
-    "name": "Aji Prasetyo",
-    "job": "Flutter Dev",
-  });
+  Uri url = Uri.parse('https://api.rajaongkir.com/starter/province');
+  final res =
+      await http.get(url, headers: {"key": "c260d509530ff505c32408a7418c79bb"});
 
   print(res.body);
 }
