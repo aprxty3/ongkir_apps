3b249e5fa967fb4a40b4c5c2ff18f2d11eb5682e
aprxty3 <ajiprxty3@gmail.com>
2022-09-10 14:23:32 +0700

    add dropdown search

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,3 +1,4 @@
+import 'package:dropdown_search/dropdown_search.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
@@ -8,16 +9,35 @@ class HomeView extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('HomeView'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
+        appBar: AppBar(
+          title: Text('Aplikasi Ongkos Kirim'),
+          centerTitle: true,
         ),
-      ),
-    );
+        body: ListView(
+          padding: const EdgeInsets.all(20),
+          children: [
+            DropdownSearch<String>(
+              popupProps: PopupProps.dialog(
+                showSelectedItems: true,
+                disabledItemFn: (String s) => s.startsWith('I'),
+              ),
+              items: ["Brazil", "Italia (Disabled)", "Tunisia", 'Canada'],
+              onChanged: print,
+              selectedItem: "Brazil",
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            DropdownSearch<String>.multiSelection(
+              items: ["Brazil", "Italia (Disabled)", "Tunisia", 'Canada'],
+              popupProps: PopupPropsMultiSelection.menu(
+                showSelectedItems: true,
+                disabledItemFn: (String s) => s.startsWith('I'),
+              ),
+              onChanged: print,
+              selectedItems: ["Brazil"],
+            )
+          ],
+        ));
   }
 }
