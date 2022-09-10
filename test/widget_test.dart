1227f497198c45d059e146165e13b93069a8f12d
aprxty3 <ajiprxty3@gmail.com>
2022-09-10 13:56:44 +0700

    refactor

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,11 +4,14 @@ import 'package:http/http.dart' as http;
 import 'package:ongkir_apps/app/data/models/user_models.dart';
 
 void main() async {
-  Uri url = Uri.parse('https://reqres.in/api/users/5');
+  Uri url = Uri.parse('https://reqres.in/api/users/10');
   final res = await http.get(url);
 
   final user =
       UserModelsResponse.fromJson(jsonDecode(res.body) as Map<String, dynamic>);
 
-  print(user.data.firstName + " " + user.data.lastName);
+  final data = user.data;
+  final supp = user.support;
+
+  print("${data.firstName} ${data.lastName}");
 }
