83dcc197a8ddcbe2a9183d6fd05e64dc3be77916
aprxty3 <ajiprxty3@gmail.com>
2022-09-11 22:24:16 +0700

    success function

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -57,9 +57,54 @@ class HomeController extends GetxController {
       default:
         berat = berat;
     }
+    print("$berat gram");
   }
 
-  void ubahSatuan(String value) {}
+  void ubahSatuan(String value) {
+    berat = double.tryParse(beratC.text) ?? 0.0;
+    switch (value) {
+      case "ton":
+        berat = berat * 1000000;
+        break;
+      case "kwintal":
+        berat = berat * 100000;
+        break;
+      case "ons":
+        berat = berat * 100;
+        break;
+      case "lbs":
+        berat = berat * 2204.62;
+        break;
+      case "pound":
+        berat = berat * 2204.62;
+        break;
+      case "kg":
+        berat = berat * 1000;
+        break;
+      case "hg":
+        berat = berat * 100;
+        break;
+      case "dag":
+        berat = berat * 10;
+        break;
+      case "gram":
+        berat = berat;
+        break;
+      case "dg":
+        berat = berat / 10;
+        break;
+      case "cg":
+        berat = berat / 100;
+        break;
+      case "mg":
+        berat = berat / 1000;
+        break;
+      default:
+        berat = berat;
+    }
+    satuan = value;
+    print("$berat gram");
+  }
 
   @override
   void onInit() {
