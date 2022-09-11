584f2009bcb83038a48de88aae81e8d12442646a
aprxty3 <ajiprxty3@gmail.com>
2022-09-11 14:12:33 +0700

    city models

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -60,6 +60,47 @@ class HomeView extends GetView<HomeController> {
               hint: "Pilih Provinsi",
               onChanged: (value) => print(value!.province),
             ),
+            SizedBox(height: 16,),
+            DropdownSearch<Province>(
+              onFind: (String filter) async {
+                Uri url =
+                Uri.parse("https://api.rajaongkir.com/starter/province");
+
+                try {
+                  final res = await http.get(
+                    url,
+                    headers: {"key": "0ae702200724a396a933fa0ca4171a7e"},
+                  );
+
+                  var data = jsonDecode(res.body) as Map<String, dynamic>;
+
+                  var status = data["rajaongkir"]["status"]["code"];
+
+                  if (status != 200) {
+                    throw data["rajaongkir"]["status"]["description"];
+                  }
+
+                  var listAllProvince =
+                  data["rajaongkir"]["results"] as List<dynamic>;
+                  var models = Province.fromJsonList(listAllProvince);
+                  return models;
+                } catch (e) {
+                  print(e);
+                  return List<Province>.empty();
+                }
+              },
+              popupItemBuilder: (context, item, isSelected) {
+                return Container(
+                  margin: EdgeInsets.all(12),
+                  child: Text("${item.province}"),
+                );
+              },
+              showSearchBox: true,
+              itemAsString: (item) => item.province!,
+              label: "Provisi Asal",
+              hint: "Pilih Provinsi",
+              onChanged: (value) => print(value!.province),
+            ),
           ],
         ));
   }
