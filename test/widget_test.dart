ad17a42400e780fd28483ed812b1e92b10507f55
aprxty3 <ajiprxty3@gmail.com>
2022-09-13 21:11:50 +0700

    test post API

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,13 +1,21 @@
-import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 import 'package:ongkir_apps/app/data/models/user_models.dart';
 
 void main() async {
-  Uri url = Uri.parse('https://api.rajaongkir.com/starter/province');
-  final res = await http.get(
+  Uri url = Uri.parse('https://api.rajaongkir.com/starter/cost');
+  final res = await http.post(
     url,
-    headers: {"key": "cf627e9dd6601da74350c46ab1f2c853"},
+    body: {
+      "origin": "501",
+      "destination": "114",
+      "weight": "1700",
+      "courier": "jne",
+    },
+    headers: {
+      "key": "cf627e9dd6601da74350c46ab1f2c853",
+      "content-type": "application/x-www-form-urlencoded",
+    },
   );
 
   print(res.body);
