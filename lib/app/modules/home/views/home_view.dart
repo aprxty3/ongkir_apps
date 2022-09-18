be52b1357f7d724706de415163c71bba39920291
aprxty3 <ajiprxty3@gmail.com>
2022-09-18 23:46:27 +0700

    success fetch

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -17,86 +17,88 @@ class HomeView extends GetView<HomeController> {
         centerTitle: true,
         backgroundColor: Colors.red[900],
       ),
-      body: ListView(
-        padding: const EdgeInsets.all(20),
-        children: [
-          ProvinsiWidget(
-            type: 'asal',
-          ),
-          Obx(() {
-            return controller.hiddenKota.isTrue
-                ? SizedBox()
-                : KotaWidget(
-                    proveId: controller.provinceId.value,
-                    type: 'asal',
-                  );
-          }),
-          ProvinsiWidget(
-            type: 'tujuan',
-          ),
-          Obx(() {
-            return controller.hiddenKotaTujuan.isTrue
-                ? SizedBox()
-                : KotaWidget(
-                    proveId: controller.provinceId.value,
-                    type: 'tujuan',
-                  );
-          }),
-          BeratWidget(),
-          Padding(
-            padding: const EdgeInsets.only(top: 16.0, bottom: 50),
-            child: DropdownSearch<Map<String, dynamic>>(
-              mode: Mode.MENU,
-              showClearButton: true,
-              items: [
-                {
-                  "code": "jne",
-                  "name": "Jalur Nugraha Ekakurir (JNE)",
-                },
-                {
-                  "code": "pos",
-                  "name": "Perusahaan Opsional Surat (POS)",
-                },
-                {
-                  "code": "tiki",
-                  "name": "Titipan Kilat (TIKI)",
-                }
-              ],
-              popupItemBuilder: (context, item, isSelected) {
-                return Container(
-                  padding: EdgeInsets.all(20),
-                  child: Text(
-                    '${item['name']}',
-                    style: TextStyle(fontSize: 18),
-                  ),
-                );
-              },
-              itemAsString: (item) => '${item['name']}',
-              onChanged: (value) {
-                if (value != null) {
-                  controller.kurir.value = value["code"];
-                  controller.showButton();
-                } else {
-                  controller.hiddenButton.value = true;
-                  controller.kurir.value = '';
-                }
-              },
-              label: 'Pilih Kurir',
+      body: SafeArea(
+        child: ListView(
+          padding: const EdgeInsets.all(20),
+          children: [
+            ProvinsiWidget(
+              type: 'asal',
             ),
-          ),
-          Obx(() {
-            return controller.hiddenButton.isTrue
-                ? SizedBox()
-                : ElevatedButton(
-                    onPressed: () => controller.ongkosKirim(),
-                    child: Text('Cek Ongkos Kirim'),
-                    style: ElevatedButton.styleFrom(
-                      padding: EdgeInsets.symmetric(vertical: 20),
-                      backgroundColor: Colors.red[900],
+            Obx(() {
+              return controller.hiddenKota.isTrue
+                  ? SizedBox()
+                  : KotaWidget(
+                      proveId: controller.provinceId.value,
+                      type: 'asal',
+                    );
+            }),
+            ProvinsiWidget(
+              type: 'tujuan',
+            ),
+            Obx(() {
+              return controller.hiddenKotaTujuan.isTrue
+                  ? SizedBox()
+                  : KotaWidget(
+                      proveId: controller.provinceId.value,
+                      type: 'tujuan',
+                    );
+            }),
+            BeratWidget(),
+            Padding(
+              padding: const EdgeInsets.only(top: 16.0, bottom: 50),
+              child: DropdownSearch<Map<String, dynamic>>(
+                mode: Mode.MENU,
+                showClearButton: true,
+                items: [
+                  {
+                    "code": "jne",
+                    "name": "Jalur Nugraha Ekakurir (JNE)",
+                  },
+                  {
+                    "code": "pos",
+                    "name": "Perusahaan Opsional Surat (POS)",
+                  },
+                  {
+                    "code": "tiki",
+                    "name": "Titipan Kilat (TIKI)",
+                  }
+                ],
+                popupItemBuilder: (context, item, isSelected) {
+                  return Container(
+                    padding: EdgeInsets.all(20),
+                    child: Text(
+                      '${item['name']}',
+                      style: TextStyle(fontSize: 18),
                     ),
                   );
-          }),
-        ],
+                },
+                itemAsString: (item) => '${item['name']}',
+                onChanged: (value) {
+                  if (value != null) {
+                    controller.kurir.value = value["code"];
+                    controller.showButton();
+                  } else {
+                    controller.hiddenButton.value = true;
+                    controller.kurir.value = '';
+                  }
+                },
+                label: 'Pilih Kurir',
+              ),
+            ),
+            Obx(() {
+              return controller.hiddenButton.isTrue
+                  ? SizedBox()
+                  : ElevatedButton(
+                      onPressed: () => controller.ongkosKirim(),
+                      child: Text('Cek Ongkos Kirim'),
+                      style: ElevatedButton.styleFrom(
+                        padding: EdgeInsets.symmetric(vertical: 20),
+                        backgroundColor: Colors.red[900],
+                      ),
+                    );
+            }),
+          ],
+        ),
       ),
     );
   }
